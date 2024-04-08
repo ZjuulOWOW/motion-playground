810bbefd10ea993d060999c03f7dff08be2e7d5e
ZjuulOWOW <zjuul@owow.io>
2024-04-08 11:51:57 +0200

    wip zoomMotion

--- main.js
+++ main.js
@@ -1,34 +1,54 @@
-import {createMouseMotion} from "./scripts/lib/createMouseMotion.js";
+import {createMouseMotion} from './lib/scripts/createMouseMotion.js'
+import {createPageTransition} from './lib/scripts/createPageTransition.js'
+import {createZoomMotion} from "./lib/scripts/createZoomMotion.js";
+
+createPageTransition({
+    duration: 800,
+    background: 'darkblue'
+})
 
 createMouseMotion('#js-mousechaser', {
     scope: '#js-container',
-    stiffness: 3,
-    ease: 'linear'
+    range: 30,
+    duration: 10000,
+    ease: 'linear',
 })
 
 createMouseMotion('#js-mousechaser2', {
     scope: '#js-container2',
-    stiffness: 3,
-    ease: 'ease-in-out',
-    easeOnLeave: 'ease-in-out'
+    range: 40,
 })
 
 createMouseMotion('#js-container2', {
     scope: '#js-container2',
-    stiffness: 3,
+    range: 30,
     ease: 'linear',
-    easeOnLeave: 'elastic'
 })
 
 createMouseMotion('#js-mousechaser3', {
     scope: '#js-container3',
-    stiffness: 1,
-    damping: 6000,
+    range: -100,
+    duration: 1000,
     ease: 'ease',
     returnOnLeave: false
 })
 
 createMouseMotion('#js-mousechaser4', {
-    stiffness: 1,
-    damping: 1000
+    range: 100,
+    duration: 1000
+})
+
+createZoomMotion('.zoom1', {
+    offset: 200,
+    direction: 'out',
+    type: 'scale',
+    scale: 2
+})
+
+createZoomMotion('.zoom2', {
+    duration: 300,
+    direction: 'out',
+    type: 'scale',
+    scale: 10,
+    offset: 900
 })
