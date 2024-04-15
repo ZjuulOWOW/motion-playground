00c3cf5925956acb1d475de4dde0c96bc6321ae5
ZjuulOWOW <zjuul@owow.io>
2024-04-15 17:11:25 +0200

    :bug: scripts not working after barba redirect WIP

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import {createMouseMotion} from './lib/scripts/createMouseMotion.js'
 import {createPageTransition} from './lib/scripts/createPageTransition.js'
-import {createZoomMotion} from "./lib/scripts/createZoomMotion.js";
+import {createZoomMotion} from "./lib/scripts/createZoomMotion.js"
 
+// page transition buiten init, zodat barba niet de hele tijd opnieuw laadt
 createPageTransition({
     duration: 500,
     direction: 'top bottom',
@@ -10,52 +11,60 @@ createPageTransition({
     customTitle: 'Custom'
 })
 
-createMouseMotion('#js-mousechaser', {
-    scope: '#js-container',
-    range: 10,
-    duration: 1000,
-    ease: 'linear',
-    use3D: true
-})
+function init(){    
+    
+    createMouseMotion('#js-mousechaser', {
+        scope: '#js-container',
+        range: 10,
+        duration: 1000,
+        ease: 'linear',
+        use3D: true
+    })
 
-createMouseMotion('#js-mousechaser2', {
-    scope: '#js-container2',
-    range: 20,
-    duration: 1600,
-    use3D: true
-})
+    createMouseMotion('#js-mousechaser2', {
+        scope: '#js-container2',
+        range: 20,
+        duration: 1600,
+        use3D: true
+    })
 
-createMouseMotion('#js-container2', {
-    scope: '#js-container2',
-    range: 10,
-    duration: 1600,
-})
+    createMouseMotion('#js-container2', {
+        scope: '#js-container2',
+        range: 10,
+        duration: 1600,
+    })
 
-createMouseMotion('#js-mousechaser3', {
-    scope: '#js-container3',
-    range: -100,
-    ease: 'ease',
-    returnOnLeave: false
-})
+    createMouseMotion('#js-mousechaser3', {
+        scope: '#js-container3',
+        range: -100,
+        ease: 'ease',
+        returnOnLeave: false
+    })
 
-createMouseMotion('#js-mousechaser4', {
-    range: 100,
-    duration: 800
-})
+    createMouseMotion('#js-mousechaser4', {
+        range: 100,
+        duration: 800
+    })
 
-createZoomMotion('.zoom1', {
-    scale: 4,
-    start: 800,
-})
+    createZoomMotion('.zoom1', {
+        scale: 4,
+        start: 800,
+    })
 
-createZoomMotion('.zoom2', {
-    direction: 'out',
-    path: '#path',
-    end: 'bottom+=1000',
-    ease: 'expo'
-})
+    createZoomMotion('.zoom2', {
+        direction: 'out',
+        path: '#path',
+        end: 'bottom+=1000',
+        ease: 'expo'
+    })
 
-createZoomMotion('.zoom3',{
-    type: 'scale',
-    scale: 40
-})
+    createZoomMotion('.zoom3',{
+        type: 'scale',
+        scale: 40
+    })
+}
+
+init()
+
+export {init}
+
