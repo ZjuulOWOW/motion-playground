332c02ae3c135f2632b739e0d60553f7d747b93d
ZjuulOWOW <zjuul@owow.io>
2024-04-25 13:46:09 +0200

    :construction: querySelectorAll in targetElement WIP

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-import { createMouseMotion } from './lib/scripts/createMouseMotion.js'
-import { createPageTransition } from './lib/scripts/createPageTransition.js'
+import { createMouseMotion } from "./lib/scripts/createMouseMotion.js"
+import { createPageTransition } from "./lib/scripts/createPageTransition.js"
 import { createZoomMotion } from "./lib/scripts/createZoomMotion.js"
 
-function init() { //createMotion()?
+function init() {
+    //createMotion()?
     // createPageTransition({
     //     sync: true,
     //     leave:[
@@ -43,116 +44,234 @@ function init() { //createMotion()?
     // ]
     // })
 
+    // createPageTransition({
+    //     leave: [
+    //     {
+    //         target: '#motion-transition',
+    //         keyframes: {
+    //             clipPath: ['inset(100% 0 0 0)', 'inset(0 0)'],
+    //         },
+    //         duration: 750,
+    //         ease: 'ease',
+    //     },
+    //     {
+    //         target: 'main',
+    //         keyframes: {
+    //             transform: 'translate3d(0,160px,0)',
+    //             opacity: 0
+    //         },
+    //         duration: 1250,
+    //         ease: 'ease',
+    //     },
+    //     {
+    //         target: '.transition-title',
+    //         keyframes: {
+    //             transform: ['translate3d(0,100%,0)', 'translate3d(0,-100%,0)'],
+    //             opacity: [0, 1, 1]
+    //         },
+    //         duration: 1250,
+    //         ease: 'cubic-bezier(0,.75,1,.25)',
+    //         delay: 750
+    //     }
+    //     ],
+    //     enter: [{
+    //         target: '.barba-wrapper',
+    //         keyframes: {
+    //             transform: 'scale(1)',
+    //         },
+    //         duration: 1250,
+    //         ease: 'ease',
+    //     },
+    //     {
+    //         target: '#motion-transition',
+    //         keyframes: {
+    //             clipPath: ['inset(0 0)', 'inset(0 0 100% 0)'],
+    //         },
+    //         duration: 750,
+    //         ease: 'ease',
+    //     },
+    //     {
+    //         target: 'main',
+    //         keyframes: {
+    //             opacity: [0, 1]
+    //         },
+    //         duration: 1250,
+    //         ease: 'ease',
+    //     }
+    //     ]
+    // })
+
+    // createPageTransition({
+    //     leave: [
+    //         {
+    //             target: "#motion-transition",
+    //             keyframes: {
+    //                 clipPath: ["inset(100% 0 0 0)", "inset(0 0)"],
+    //             },
+    //             duration: 750,
+    //             ease: "ease",
+    //         },
+    //         {
+    //             target: "main",
+    //             keyframes: {
+    //                 transform: "translate3d(0,160px,0)",
+    //                 opacity: 0,
+    //             },
+    //             duration: 1000,
+    //             ease: "ease",
+    //         },
+    //         {
+    //             target: ".transition-title",
+    //             keyframes: {
+    //                 transform: [
+    //                     "translate3d(0,100%,0)",
+    //                     "translate3d(0,-100%,0)",
+    //                 ],
+    //                 opacity: [0, 1, 1],
+    //             },
+    //             duration: 750,
+    //             ease: "cubic-bezier(0,.5,1,.5)",
+    //             delay: 500,
+    //         },
+    //     ],
+    //     enter: [
+    //         {
+    //             target: "#motion-transition",
+    //             keyframes: {
+    //                 clipPath: ["inset(0 0)", "inset(0 0 100% 0)"],
+    //             },
+    //             duration: 750,
+    //             ease: "ease",
+    //         },
+    //         {
+    //             target: "main",
+    //             keyframes: {
+    //                 transform: ["scale(.8)", "scale(1)"],
+    //             },
+    //             duration: 800,
+    //             ease: "ease",
+    //         },
+    //     ],
+    // })
+
     createPageTransition({
-        leave: [{
-            target: '.barba-wrapper',
-            keyframes: {
-                transform: 'scale(.5)',
+        leave: [
+            {
+                target: "#motion-transition",
+                keyframes: {
+                    clipPath: ["inset(100% 0 0 0)", "inset(0 0)"],
+                },
+                duration: 750,
+                ease: "ease",
             },
-            duration: 2000,
-            ease: 'ease-out',
-        },
-        {
-            target: '#motion-transition',
-            keyframes: {
-                clipPath: ['inset(100% 0 0 0)', 'inset(0 0)'],
+            {
+                target: "main",
+                keyframes: {
+                    transform: "translate3d(0,160px,0)",
+                    opacity: 0,
+                },
+                duration: 1000,
+                ease: "ease",
             },
-            duration: 750,
-            ease: 'ease',
-        },
-        {
-            target: 'main',
-            keyframes: {
-                transform: 'translate3d(0,160px,0)',
-                opacity: 0
+            {
+                target: ".transition-title",
+                keyframes: {
+                    transform: [
+                        "translate3d(0,100%,0)",
+                        "translate3d(0,-100%,0)",
+                    ],
+                    opacity: [0, 1, 1],
+                },
+                duration: 750,
+                ease: "cubic-bezier(0,.5,1,.5)",
+                delay: 500,
             },
-            duration: 1250,
-            ease: 'ease',
-        },
-        {
-            target: '.transition-title',
-            keyframes: {
-                transform: ['translate3d(0,100%,0)', 'translate3d(0,-100%,0)'],
-                opacity: [0, 1, 1]
-            },
-            duration: 1250,
-            ease: 'cubic-bezier(0,.75,1,.25)',
-            delay: 750
-        }
         ],
-        enter: [{
-            target: '.barba-wrapper',
-            keyframes: {
-                transform: 'scale(1)',
+        enter: [
+            {
+                target: "#motion-transition",
+                keyframes: {
+                    clipPath: ["inset(0 0)", "inset(0 0 100% 0)"],
+                },
+                duration: 750,
+                ease: "ease",
             },
-            duration: 1250,
-            ease: 'ease',
-        },
-        {
-            target: '#motion-transition',
-            keyframes: {
-                clipPath: ['inset(0 0)', 'inset(0 0 100% 0)'],
+            {
+                target: "main",
+                keyframes: {
+                    transform: ["scale(.8)", "scale(1)"],
+                },
+                duration: 800,
+                ease: "ease",
             },
-            duration: 750,
-            ease: 'ease',
-        },
-        {
-            target: 'main',
-            keyframes: {
-                opacity: [0, 1]
-            },
-            duration: 1250,
-            ease: 'ease',
-        }
-        ]
+        ],
     })
 
-    createMouseMotion('#js-mousechaser', {
-        scope: '#js-container',
+    // createPageTransition({
+    //     leave: {
+    //         target: '.title',
+    //         keyframes: {
+    //             transform: "translate3d(0,100vh,0)",
+    //         },
+    //         duration: 3000,
+    //         delay: 3000,
+    //     },
+    //     enter: {
+    //         target: '.title',
+    //         keyframes: {
+    //             transform: "translate3d(0,100vh,0)",
+    //         },
+    //         duration: 3000,
+    //         delay: 3000,
+    //     },
+    // })
+
+    createMouseMotion("#js-mousechaser", {
+        scope: "#js-container",
         range: 90,
         duration: 1000,
-        ease: 'linear',
-        use3D: true
+        ease: "linear",
+        use3D: true,
     })
 
-    createMouseMotion('#js-mousechaser2', {
-        scope: '#js-container2',
+    createMouseMotion("#js-mousechaser2", {
+        scope: "#js-container2",
         range: 20,
         duration: 1600,
-        use3D: true
+        use3D: true,
     })
 
-    createMouseMotion('#js-container2', {
-        scope: '#js-container2',
+    createMouseMotion("#js-container2", {
+        scope: "#js-container2",
         range: 10,
         duration: 1600,
     })
 
-    createMouseMotion('#js-mousechaser3', {
-        scope: '#js-container3',
+    createMouseMotion("#js-mousechaser3", {
+        scope: "#js-container3",
         range: -100,
-        ease: 'ease',
+        ease: "ease",
         returnOnLeave: false,
-        duration: 800
+        duration: 800,
     })
 
-    createMouseMotion('#js-mousechaser4', {
+    createMouseMotion("#js-mousechaser4", {
         range: 100,
-        duration: 800
+        duration: 800,
     })
 
-    createZoomMotion('.zoom1', {
+    createZoomMotion(".zoom1", {
         scale: 4,
         start: 800,
     })
 
-    createZoomMotion('.zoom2-mask', {
-        direction: 'in',
-        clipContainer: '.zoom2-container',
+    createZoomMotion(".zoom2-mask", {
+        direction: "in",
+        clipContainer: ".zoom2-container",
         start: 400,
-        end: 'bottom+=2000',
-        ease: 'expo',
-        scale: 4
+        end: "bottom+=2000",
+        ease: "expo",
+        scale: 4,
     })
 
     // createZoomMotion('.zoom3',{
