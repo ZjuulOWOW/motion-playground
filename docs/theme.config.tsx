f1d7238beb27a379e9992db898044456e74f8156
ZjuulOWOW <zjuul@owow.io>
2024-05-16 17:09:33 +0200

    :sparkles: added docs, wip secondorderdynamics

---
+++ docs/theme.config.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>OWOW-motion</span>,
+  project: {
+    link: 'https://github.com/owowagency/gsap-motion',
+  },
+  docsRepositoryBase: 'https://github.com/owowagency/gsap-motion'
+}
+
+export default config
